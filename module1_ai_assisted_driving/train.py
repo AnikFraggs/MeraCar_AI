0e72b0d83861e8147459bec581369fae4def6af9
AnikFraggs <anikrik06@gmail.com>
2026-07-02 11:44:06 +0530

    Refactor import statements in train.py
    Updated import statements to remove relative paths.

--- module1_ai_assisted_driving/train.py
+++ module1_ai_assisted_driving/train.py
@@ -4,8 +4,8 @@ import joblib
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from ..common.utils import set_seed, get_device, module_dir, ensure_dirs, setup_logger
-from ..common.torch_helper import make_loaders, train_regression, save_checkpoint
+from common.utils import set_seed, get_device, module_dir, ensure_dirs, setup_logger
+from common.torch_helper import make_loaders, train_regression, save_checkpoint
 from . import data_gen, model as M
 
 logger = setup_logger("Module1_Train")
